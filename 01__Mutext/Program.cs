27c7207d73d8c9b1212b79684bd79629901b4d19
Pavlo Marchuk <Pavlo Marchuk@DESKTOP-MQBICKJ>
2017-06-06 09:37:51 +0300

    06.06 Mutext 1

---
+++ 01__Mutext/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _01__Mutext
+	{
+	class Program
+		{
+		static Mutex mutex;
+		static void Main(string[] args)
+			{
+
+			try
+				{
+				mutex = Mutex.OpenExisting("MY_MUTEXT");
+				}
+			catch(Exception e)
+				{
+				Console.WriteLine(e.Message);				
+				}
+			if(mutex != null)
+				{
+				Console.WriteLine("Вже чогось працює");
+				Console.ReadKey();
+				return;
+				}
+
+			// Мютекс запкскає гарантовано один екземпляр
+			using(var mutex = new Mutex(true, "MY_MUTEXT"))
+				{
+				Console.WriteLine("mutex працює");
+				Console.ReadKey();
+				}
+
+			}
+		}
+	}
